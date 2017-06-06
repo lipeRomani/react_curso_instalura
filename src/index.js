c0a2f81296a8fc85645c7065cb53b91c00478071
Felipe Romani <felipe.romani@wideti.com.br>
2017-06-06 12:51:46 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import "./css/login.css";
+import "./css/reset.css";
+import "./css/timeline.css";
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+    );
+registerServiceWorker();
