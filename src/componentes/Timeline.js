962ac94f5a503143abbde6274b3223d91f60a755
Felipe Romani <felipe.romani@wideti.com.br>
2017-06-21 17:30:56 -0300

    CONTINUANDO

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import FotoItem from './FotoItem';
 
 export default class Timeline extends Component {
+    
+    constructor()
+    {
+        super();
+        this.state = {fotos : []};
+    }
+
+    componenteDidMount()
+    {
+        fetch('http://localhost:8080/api/public/fotos/alots')
+        .then(resp => resp.json())
+        .then(fotos => {
+            this.setState({fotos : fotos});
+        })
+        .catch(err => {
+
+        });
+    }
+    
     render(){
         return (
         <div className="fotos container">
